a0702426abce2c31b0ee31e9499d5ac65e360ca4
felipoxy1 <luizfelipepassos2015@gmail.com>
2025-05-27 22:46:39 -0300

    test: adicionando teste para a insercao de comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -3,8 +3,28 @@ import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve adicionar dois comentarios', () => {
+        render(<PostComment/>)
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Test-1'
+            }
+        })
+        fireEvent.click(screen.getByTestId('comment-button'))
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Test-2'
+            }
+        })
+        fireEvent.click(screen.getByTestId('comment-button'))
+
+        expect(screen.getByText('Test-2')).toBeInTheDocument()
+    })
 });
